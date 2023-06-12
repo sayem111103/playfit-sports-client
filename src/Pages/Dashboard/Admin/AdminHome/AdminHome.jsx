30cc59dff6e0815e903bf19ee8e4b2630e860823
sayem111103 <sayemul03@gmail.com>
2023-06-12 20:29:16 +0600

    user home add

--- src/Pages/Dashboard/Admin/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/Admin/AdminHome/AdminHome.jsx
@@ -1,7 +1,18 @@
+import useAuth from "../../../../Hooks/useAuth";
+
 const AdminHome = () => {
+    const { user } = useAuth();
     return (
-        <div>
-            this is admin home
+        <div className="text-center flex-col flex items-center mt-5 font-extrabold">
+            Welcome {user?.name || 'Dear User'}
+            <>
+                <div className="avatar mt-3">
+                    <div className="w-24 rounded">
+                        <img src={user?.photoURL} />
+                    </div>
+                </div>
+            </>
+
         </div>
     );
 };
